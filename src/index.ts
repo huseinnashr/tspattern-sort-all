13f8986152b2a4296ce2212c789c6586772710a3
Husein <nvdhunter007@gmail.com>
2020-06-06 22:53:04 +0700

    use abstract class instead

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-import sorterFunction from "./SorterFunction";
+// import sorterFunction from "./SorterFunction";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-sorterFunction(numbersCollection);
+// sorterFunction(numbersCollection);
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaaa");
-sorterFunction(charactersCollection);
+// sorterFunction(charactersCollection);
+charactersCollection.sort();
 console.log(charactersCollection.data);
